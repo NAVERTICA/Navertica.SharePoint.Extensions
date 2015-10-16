eacc0460671cc9b8db031186d01d1fadca795972
jmatysek <jaromir.matysek@navertica.com>
2015-10-16 15:47:05 +0200

    DisabledItemEventsScope - scope to prevent event firing when updating SPListItems

---
+++ Classes/DisabledItemEventsScope.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using Microsoft.SharePoint;
+
+namespace Navertica.SharePoint
+{
+    /// <summary>
+    /// Use this scope to prevent event firing when updating SPListItems
+    /// using (var scope = DisabledItemEventsScope()) { item.Update(); }
+    /// via https://adrianhenke.wordpress.com/2010/01/29/disable-item-events-firing-during-item-update/
+    /// </summary>
+    public sealed class DisabledItemEventsScope : SPItemEventReceiver, IDisposable
+    {
+        private readonly bool oldValue;
+
+        public bool Enabled
+        {
+            get { return base.EventFiringEnabled; }
+        }
+
+        public DisabledItemEventsScope()
+        {
+            oldValue = base.EventFiringEnabled;
+            base.EventFiringEnabled = false;
+        }
+
+        public void Dispose()
+        {
+            base.EventFiringEnabled = oldValue;
+        }
+    }
+}
